d514ae37b446f07f4ffc9ad9ec41df7c55727f1c
farid <mohdfarid.jalil@sapuraenergy.com>
2021-12-01 01:42:37 +0800

    first commit

---
+++ parallax_mountain.js
@@ -0,0 +1,34 @@
+var onion = document.getElementById('onion');
+var container = document.getElementById('container');
+
+var layerOne = document.getElementById("layerOne");
+var layerOne = document.getElementById("layerOne");
+
+var mouseX = 0;
+var mouseY = 0;
+
+var centerScreen = (1200 / 2) - (1200 / 2);
+// this var can be used in the -mouseX as variable
+
+window.addEventListener("mousemove", onMouseMove);
+//(type of event, listener(func name))
+
+function onMouseMove(event) {
+    console.log(event.pageX);
+
+    mouseX = event.pageX - container.offsetLeft;
+    mouseY = event.pageY;
+
+    onion.style.left = mouseX - 25 + 'px';
+    onion.style.top = mouseY - 25 + 'px';
+
+    layerOne.style.left = (-mouseX * 0.08) + (1000 / 2 - 1200 / 2) + "px";
+    layerTwo.style.left = (-mouseX * 0.05) + (1000 / 2 - 1200 / 2) + "px";
+    layerThree.style.left = (-mouseX * 0.02) + centerScreen + "px";
+    layerFour.style.left = (-mouseX * 0.02) + (1000 / 2 - 1200 / 2) + "px";
+    layerFive.style.left = (-mouseX * 0.02) + (1000 / 2 - 1200 / 2) + "px";
+    layerSix.style.left = (-mouseX * 0.02) + (1000 / 2 - 1200 / 2) + "px";
+
+    //lower no lower gap 0.05, 0.08
+}
+
---
+++ timer.js
@@ -0,0 +1,44 @@
+//2 type od timer
+//1. setInterval(function,time in milisec)
+//- funtion moveBox
+
+var leftPos = 0;
+
+var topPos = 0;
+
+// setInterval(moveBox, 10);
+// open back if want to use move only without timer
+
+function moveBox() {
+    // console.log("Is moving!");
+
+    if (leftPos > 900) {
+        leftPos = -100;
+    }
+
+    leftPos += 5;// pixel no
+
+    document.getElementById('box1').style.left = leftPos + "px";// console.log("leftpos", leftPos);
+
+
+
+    if (topPos > 600) {
+        topPos = -100;
+    }
+
+    topPos += 5;// pixel no
+
+    document.getElementById('box2').style.top = topPos + "px";// console.log("toppos", topPos);
+
+}
+
+//2. countdown time
+//-setTimeout(countDown,time in milisec)
+
+setTimeout(countDown, 5000)
+
+function countDown() {
+    console.log("GO!!!");
+    setInterval(moveBox, 10);
+}
+
