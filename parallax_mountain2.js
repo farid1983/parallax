02ce7c1d731437ac58d6b855b26afec66a39a236
farid <mohdfarid.jalil@sapuraenergy.com>
2021-12-01 08:10:10 +0800

    update mountain

--- parallax_mountain2.js
+++ parallax_mountain2.js
@@ -1,18 +1,17 @@
 var onion = document.getElementById('onion');
 var container = document.getElementById('container');
 
-var layerZero = document.getElementById("layerZero");
-var layerOne = document.getElementById("layerOne");
-var layerTwo = document.getElementById("layerTwo");
-var layerThree = document.getElementById("layerThree");
-var layerFour = document.getElementById("layerFour");
-var layerFive = document.getElementById("layerFive");
-var layerSix = document.getElementById("layerSix");
+var layerZero = document.getElementById("person");
+var layerOne = document.getElementById("image");
+var layerTwo = document.getElementById("mountain1");
+var layerThree = document.getElementById("mountain2");
+var layerFour = document.getElementById("mountain3");
+var layerFive = document.getElementById("sky");
 
 var mouseX = 0;
 var mouseY = 0;
 
-var centerScreen = (1200 / 2) - (1200 / 2);
+var centerScreen = (1000 / 2) - (1000 / 2);
 // this var can be used in the -mouseX as variable
 
 window.addEventListener("mousemove", onMouseMove);
@@ -27,13 +26,13 @@ function onMouseMove(event) {
     onion.style.left = mouseX - 25 + 'px';
     onion.style.top = mouseY - 25 + 'px';
 
-    layerZero.style.left = (-mouseX * 0.02) + (1000 / 2 - 1200 / 2) + "px";
-    layerOne.style.left = (-mouseX * 0.03) + (1000 / 2 - 1200 / 2) + "px";
-    layerTwo.style.left = (-mouseX * 0.04) + (1000 / 2 - 1200 / 2) + "px";
+    layerZero.style.left = (-mouseX * 0.02) + (1000 / 2 - 1000 / 2) + "px";
+    layerOne.style.left = (-mouseX * 0.03) + (1000 / 2 - 1000 / 2) + "px";
+    layerTwo.style.left = (-mouseX * 0.04) + (1000 / 2 - 1000 / 2) + "px";
     layerThree.style.left = (-mouseX * 0.05) + centerScreen + "px";
-    layerFour.style.left = (-mouseX * 0.06) + (1000 / 2 - 1200 / 2) + "px";
-    layerFive.style.left = (-mouseX * 0.07) + (1000 / 2 - 1200 / 2) + "px";
-    layerSix.style.left = (-mouseX * 0.08) + (1000 / 2 - 1200 / 2) + "px";
+    layerFour.style.left = (-mouseX * 0.06) + (1000 / 2 - 1000 / 2) + "px";
+    layerFive.style.left = (-mouseX * 0.07) + (1000 / 2 - 1000 / 2) + "px";
+
 
     //lower no lower gap 0.05, 0.08
 }
